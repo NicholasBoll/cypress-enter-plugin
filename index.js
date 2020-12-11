9ac3c980c15d245650f4d64a8643196bd59fa620
Nicholas Boll <nicholas.boll@gmail.com>
2020-12-11 16:30:34 -0700

    fix: Fix enter key on anchors Fixes #2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ Cypress.Commands.overwrite("type", (originalFn, subject, text, options) => {
   return originalFn(subject, text, options).then((subject) => {
     const tagName = subject[0].tagName.toLowerCase();
     if (text.includes("{enter}") && (tagName === "button" || tagName === "a")) {
-      subject.click();
+      subject[0].click();
     }
 
     return subject;
